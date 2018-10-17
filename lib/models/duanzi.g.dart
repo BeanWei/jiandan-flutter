b6eee486a7451a137e62aa93de5d7f8a10bc8ba7
BeanWei <157318439@qq.com>
2018-10-17 22:24:59 +0800

    json序列化

---
+++ lib/models/duanzi.g.dart
@@ -0,0 +1,60 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'duanzi.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DuanZi _$DuanZiFromJson(Map<String, dynamic> json) {
+  return DuanZi(
+      status: json['status'] as String,
+      current_page: json['current_page'] as int,
+      total_comments: json['total_comments'] as int,
+      page_count: json['page_count'] as int,
+      count: json['count'] as int,
+      comments: (json['comments'] as List)
+          ?.map((e) => e == null
+              ? null
+              : DuanZiComments.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$DuanZiToJson(DuanZi instance) => <String, dynamic>{
+      'status': instance.status,
+      'current_page': instance.current_page,
+      'total_comments': instance.total_comments,
+      'page_count': instance.page_count,
+      'count': instance.count,
+      'comments': instance.comments
+    };
+
+DuanZiComments _$DuanZiCommentsFromJson(Map<String, dynamic> json) {
+  return DuanZiComments(
+      comment_ID: json['comment_ID'] as String,
+      comment_post_ID: json['comment_post_ID'] as String,
+      comment_author: json['comment_author'] as String,
+      comment_date: json['comment_date'] as String,
+      comment_date_gmt: json['comment_date_gmt'] as String,
+      comment_content: json['comment_content'] as String,
+      user_id: json['user_id'] as String,
+      vote_positive: json['vote_positive'] as String,
+      vote_negative: json['vote_negative'] as String,
+      sub_comment_count: json['sub_comment_count'] as String,
+      text_content: json['text_content'] as String);
+}
+
+Map<String, dynamic> _$DuanZiCommentsToJson(DuanZiComments instance) =>
+    <String, dynamic>{
+      'comment_ID': instance.comment_ID,
+      'comment_post_ID': instance.comment_post_ID,
+      'comment_author': instance.comment_author,
+      'comment_date': instance.comment_date,
+      'comment_date_gmt': instance.comment_date_gmt,
+      'comment_content': instance.comment_content,
+      'user_id': instance.user_id,
+      'vote_positive': instance.vote_positive,
+      'vote_negative': instance.vote_negative,
+      'sub_comment_count': instance.sub_comment_count,
+      'text_content': instance.text_content
+    };
---
+++ lib/models/news.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'news.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+News _$NewsFromJson(Map<String, dynamic> json) {
+  return News(
+      status: json['status'] as String,
+      count: json['count'] as int,
+      count_total: json['count_total'] as int,
+      pages: json['pages'] as int,
+      posts: (json['posts'] as List)
+          ?.map((e) =>
+              e == null ? null : NewsPosts.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$NewsToJson(News instance) => <String, dynamic>{
+      'status': instance.status,
+      'count': instance.count,
+      'count_total': instance.count_total,
+      'pages': instance.pages,
+      'posts': instance.posts
+    };
+
+NewsPosts _$NewsPostsFromJson(Map<String, dynamic> json) {
+  return NewsPosts(
+      id: json['id'] as String,
+      url: json['url'] as String,
+      title: json['title'] as String,
+      excerpt: json['excerpt'] as String,
+      date: json['date'] as String,
+      comment_count: json['comment_count'] as String,
+      custom_fields: json['custom_fields'] == null
+          ? null
+          : CustomFields.fromJson(
+              json['custom_fields'] as Map<String, dynamic>));
+}
+
+Map<String, dynamic> _$NewsPostsToJson(NewsPosts instance) => <String, dynamic>{
+      'id': instance.id,
+      'url': instance.url,
+      'title': instance.title,
+      'excerpt': instance.excerpt,
+      'date': instance.date,
+      'comment_count': instance.comment_count,
+      'custom_fields': instance.custom_fields
+    };
+
+CustomFields _$CustomFieldsFromJson(Map<String, dynamic> json) {
+  return CustomFields(thumb_c: json['thumb_c'] as List);
+}
+
+Map<String, dynamic> _$CustomFieldsToJson(CustomFields instance) =>
+    <String, dynamic>{'thumb_c': instance.thumb_c};
