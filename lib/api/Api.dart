1f647f10be00c4c4212e1a558bab92fad55af9ab
BeanWei <157318439@qq.com>
2018-10-17 22:24:01 +0800

    更新Api接口定义

---
+++ lib/api/Api.dart
@@ -0,0 +1,15 @@
+
+Map JianDanApis = {
+
+  '妹子图': 'http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments&page=@page',
+
+  '无聊图': 'http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments&page=@page',
+
+  '段子手': 'http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_duan_comments&page=@page',
+
+  '新鲜事': 'http://jandan.net/?oxwlxojflwblxbsapi=get_recent_posts&include=url,date,title,excerpt,comment_count,custom_fields&custom_fields=thumb_c&page=@page',
+
+  'news_detail': 'http://jandan.net/?oxwlxojflwblxbsapi=get_post&include=content&id=@id',
+
+  'news_comments': 'http://jandan.net/?oxwlxojflwblxbsapi=get_post&include=comments&id=@id'
+};
