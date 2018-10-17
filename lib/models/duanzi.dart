d62a2d3163f9495a777ba4ef50a4f437ac17a683
BeanWei <157318439@qq.com>
2018-10-17 16:42:31 +0800

    添加duanzi model

--- lib/models/duanzi.dart
+++ lib/models/duanzi.dart
@@ -1,34 +1,54 @@
+import 'dart:core';
+
+import 'package:json_annotation/json_annotation.dart';
+part 'duanzi.g.dart';
+
+
+@JsonSerializable()
 class DuanZi {
+  String status;
+  int current_page;
+  int total_comments;
+  int page_count;
+  int count;
+  List<Comments> comments;
   DuanZi({
-    this.author,
-    this.positive,
-    this.negative,
-    this.comment,
-    this.content,
-  });
-
-  final String author;
-  final String positive;
-  final String negative;
-  final String comment;
-  final String content;
-
-  @override
-  bool operator == (Object other) {
-    identical(this, other) ||
-        other is DuanZi &&
-            author == other.author &&
-            positive == other.positive &&
-            negative == other.negative &&
-            comment == other.comment &&
-            content == other.content;
-  }
-
-  @override
-  int get hashCode =>
-      author.hashCode ^
-      positive.hashCode ^
-      negative.hashCode ^
-      comment.hashCode ^
-      content.hashCode;
+    this.status,
+    this.current_page,
+    this.total_comments,
+    this.page_count, this.count,
+    this.comments});
+  factory DuanZi.fromJson(Map<String, dynamic> json) => _$DuanZiFromJson(json);
+  Map<String, dynamic> toJson() => _$DuanZiToJson(this);
 }
+
+
+@JsonSerializable()
+class Comments {
+  String comment_ID;
+  String comment_post_ID;
+  String comment_author;
+  String comment_date;
+  String comment_date_gmt;
+  String comment_content;
+  String user_id;
+  String vote_positive;
+  String vote_negative;
+  String sub_comment_count;
+  String text_content;
+  Comments({
+    this.comment_ID,
+    this.comment_post_ID,
+    this.comment_author,
+    this.comment_date,
+    this.comment_date_gmt,
+    this.comment_content,
+    this.user_id,
+    this.vote_positive,
+    this.vote_negative,
+    this.sub_comment_count,
+    this.text_content,});
+  factory Comments.fromJson(Map<String, dynamic> json) => _$CommentsFromJson(json);
+  Map<String, dynamic> toJson() => _$CommentsToJson(this);
+}
+
