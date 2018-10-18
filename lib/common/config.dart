dc6956f0948f16ace31dc1a0a8bef77698b28919
BeanWei <157318439@qq.com>
2018-10-18 16:07:10 +0800

    添加主题参数

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/foundation.dart';
+
+
 bool networkEnable = true;
+
+
+
+enum ThemeType { light, dark, brown, blue }
+
+class JianDanTheme {
+  ThemeType themeType;
+
+  JianDanTheme(
+    {@required this.themeType}) : assert(themeType != null);
+
+  JianDanTheme copyWith({
+    ThemeType themeType
+  }) {
+    return JianDanTheme(
+      themeType: themeType ?? this.themeType
+    );
+  }
+}
